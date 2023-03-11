0b48054cd5adae5bac2ee37ba7e315558c116b27
Tara Mazaya Lababan <tara.mazaya@ui.ac.id>
2023-03-11 11:12:46 +0700

    commit

--- my_app/repositories.py
+++ my_app/repositories.py
@@ -6,34 +6,34 @@ from . import models, schemas
 
 class ItemRepo:
     
- async def create(db: Session, item: schemas.ItemCreate):
+    async def create(db: Session, item: schemas.ItemCreate):
         db_item = models.Item(name=item.name,
-                              price=item.price,
-                              recipe_id=item.recipe_id)
+                                price=item.price,
+                                recipe_id=item.recipe_id)
         db.add(db_item)
         db.commit()
         db.refresh(db_item)
         return db_item
     
- def fetch_by_id(db: Session,_id):
-     return db.query(models.Item).filter(models.Item.id == _id).first()
+    def fetch_by_id(db: Session,_id):
+        return db.query(models.Item).filter(models.Item.id == _id).first()
  
- def fetch_by_name(db: Session,name):
-     return db.query(models.Item).filter(models.Item.name == name).first()
- 
- def fetch_all(db: Session, skip: int = 0, limit: int = 100):
-     return db.query(models.Item).offset(skip).limit(limit).all()
- 
- async def delete(db: Session,item_id):
-     db_item= db.query(models.Item).filter_by(id=item_id).first()
-     db.delete(db_item)
-     db.commit()
+    def fetch_by_name(db: Session,name):
+        return db.query(models.Item).filter(models.Item.name.ilike(name)).first()
+
+    def fetch_all(db: Session, skip: int = 0, limit: int = 100):
+        return db.query(models.Item).offset(skip).limit(limit).all()
+
+    async def delete(db: Session,item_id):
+        db_item= db.query(models.Item).filter_by(id=item_id).first()
+        db.delete(db_item)
+        db.commit()
      
      
- async def update(db: Session,item_data):
-    updated_item = db.merge(item_data)
-    db.commit()
-    return updated_item
+    async def update(db: Session,item_data):
+        updated_item = db.merge(item_data)
+        db.commit()
+        return updated_item
     
     
     
@@ -53,7 +53,7 @@ class RecipeRepo:
         return db.query(models.Recipe).filter(models.Recipe.id == _id).first()
     
     def fetch_by_name(db: Session,name:str):
-        return db.query(models.Recipe).filter(models.Recipe.name == name).first()
+        return db.query(models.Recipe).filter(models.Recipe.name.ilike(name)).first()
     
     def fetch_all(db: Session, skip: int = 0, limit: int = 100):
         return db.query(models.Recipe).offset(skip).limit(limit).all()
